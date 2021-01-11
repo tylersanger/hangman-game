ac58ec169bcc0c21b7f229d817719a05c2e67711
Tyler <t.sanger1992@gmail.com>
2021-01-10 21:23:00 -0600

    Working on client/server implementation

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+IP = 'localhost'
+port = 55987
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((IP,port))
+
+while True:
+    msg = input("=> ")
+    s.send(msg.encode('ascii'))
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 import socket
-from hangman import play
+from hangman import Hangman
 import json
+import threading
 
 IP = 'localhost'
 port = 55987
-players = {} 
+players = {}
+
+def handle_client(conn,addr):
+    msg = ''
+
+    while msg != 'q':
+        msg = conn.recv(1024).decode('ascii')
+    
+    conn.close()
+
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((IP,port))
 s.listen()
 
-conn,addr = s.accept()
+while True:
 
-players[]
+    conn,addr = s.accept()
+    players[addr[0]] = list((list(),0))
+    threading._start_new_thread(handle_client,(conn,addr))
+
+
+
 
