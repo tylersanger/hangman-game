2bbaac53d95bd6acd545fdbb2f781eebcc300455
Tyler <t.sanger1992@gmail.com>
2022-08-14 17:38:21 -0500

    Merged hangman.py from master

--- hangman.py
+++ hangman.py
@@ -1,137 +1,194 @@
+"""System Modules"""
 import json
 import random
 from customException import InvalidInputHangmanError, ResponseLengthError
-import os
 
 
 class Hangman:
+    """Class to handle the Hangman game functionality.
+
+    Public Methods:
+        __init__: Initializes the game.
+        __str__: Overloads the __str__ method to print the current state of the Hangman game.
+        check_guess: Checks whether the user guess was correct.
+        get_game_state: Gets the current state of letter holder.
+        __check_for_winner: Checks whether the user has won the game.
+        play: Sets up the game and begins the game loop.
+        get_word_to_guess: Returns the word to be guessed.
+
+    Private Methods:
+        __get_words_and_word_count: gets a list of words and the number of
+                                    words in the pool to guess from.
+        __prepare_game: Sets up the letter_holder variable.
+        __add_guess_to_list: Adds a guessed letter or word to the used letter or word list.
+    """
+
     def __init__(self):
         self.__words = []
         self.__count = 0
-        self.__letterHolder = []
+        self.__letter_holder = []
         self.__strikes = 0
-        self.__usedLetters = []
-        self.__usedWords = []
-        self.__wordToGuess = []
+        self.__used_letters = []
+        self.__used_words = []
+        self.__word_to_guess = []
 
     def __str__(self):
 
         temp = ""
 
-        for letter in self.__letterHolder:
+        for letter in self.__letter_holder:
             temp += letter + " "
 
-        return temp
+        temp += "\n"
 
-    def __get_words_and_wordCount(self):
+        return str("You have " + str(self.__strikes) + " strikes.\n" + str(temp))
 
-        """Returns a list of words and the number of words in the list.
+    def __get_words_and_word_count(self) -> tuple[str,int]:
+        """
+        Gets the list of words and the number of words in the pool to guess from.
+        """
 
-        Returns:
-        words: List of words to choose from
-        count: Number of words in the list"""
+        file = open("wordlist.json", "r", encoding="ascii")
 
-        f = open("wordlist.json", "r")
-
-        contents = json.load(f)
+        contents = json.load(file)
         words = contents["words"]
         count = len(words)
 
-        f.close()
+        file.close()
 
         return words, count
 
-    def __prepare_game(self, word):
+    def __prepare_game(self, word) -> str:
+        """
+        Takes in the word to guess and sets up the letter_holder variable based off it.
 
-        letterHolder = list()
+        Args:
+            word (str): The word to guess.
 
-        for letter in range(0, len(word)):
+        Returns:
+            letter_holder (str): Variable with as many underscore's as there
+                                 are letters in the word to guess.
+        """
 
-            if word[letter] == "-" or word[letter] == "'":
-                letterHolder.append(word[letter])
+        letter_holder = list()
+
+        for index,_ in enumerate(word):
+
+            if word[index] == "-" or word[index] == "'":
+                letter_holder.append(word[index])
 
             else:
-                letterHolder.append("_")
+                letter_holder.append("_")
 
-        return letterHolder
+        return letter_holder
 
-    def check_guess(self, guess):
+    def check_guess(self, guess) -> bool:
+        """
+        Checks to see if the guess was first valid and then sees if the guess is correct.
 
-        lengthOfGuess = len(guess)
-        lengthOfWord = len(self.__wordToGuess)
-        guessedCorrectly = False
-        gameWon = False
+        Args:
+            guess (str): The guess to check.
+        Returns:
+            bool: True if the guess is correct, False otherwise.
+        """
 
-        if lengthOfGuess < 1:
+        length_of_guess = len(guess)
+        length_of_word = len(self.__word_to_guess)
+        guessed_correctly = False
+        game_won = False
+
+        if length_of_guess < 1:
             raise ResponseLengthError(
-                "Guess must be at least one character in length. Please try again.\n"
+                "Guess must be at least one character in length." \
+                   " Please try again."
             )
 
-        for letter in range(0, lengthOfGuess):
+        for letter in range(length_of_guess):
 
-            if ord(guess[letter].upper()) < ord("A") or ord(
-                guess[letter].upper()
-            ) > ord("Z"):
+            if ord(guess[letter].upper()) < 65 or ord(guess[letter].upper()) > 90:
                 raise InvalidInputHangmanError(
-                    "Invalid input. Must be a character or word with no special characters. Please try again.\n"
+                    "Invalid input. Must be a alphabetical character or word with no special" \
+                        " characters. Please try again."
                 )
 
-        if lengthOfGuess == 1:
-
-            if guess in self.__usedLetters:
+        if length_of_guess == 1:
+            if guess in self.__used_letters:
+                print(f'Guess "{guess}" has already been used.')
                 self.__strikes += 1
-                return gameWon
+                return game_won
 
-            for letter in range(0, lengthOfWord):
+            for letter in range(length_of_word):
+                if guess == self.__word_to_guess[letter]:
+                    guessed_correctly = True
+                    self.__letter_holder[letter] = guess
 
-                if guess == self.__wordToGuess[letter]:
-                    guessedCorrectly = True
-                    self.__letterHolder[letter] = guess
-
-            if not guessedCorrectly:
+            if guessed_correctly:
+                game_won = self.__check_for_winner(None,None,self.__letter_holder)
+            else:
                 self.__strikes += 1
-                self.__add_guess_to_list(guess)
-                return gameWon
-
             self.__add_guess_to_list(guess)
 
-            return self.check_for_winner(guess, self.__wordToGuess, self.__letterHolder)
+            return game_won
 
-        elif guess in self.__usedWords:
+        if guess in self.__used_words:
             self.__strikes += 1
-            return gameWon
+            print(f'Guess "{guess}" has already been used.')
+            return game_won
 
-        else:
-            gameWon = self.check_for_winner(guess, self.__wordToGuess, None)
+        game_won = self.__check_for_winner(guess, self.__word_to_guess, None)
 
-            if gameWon:
-                return gameWon
+        if not game_won:
+            self.__add_guess_to_list(guess)
+            self.__strikes += 1
+            return game_won
 
-            else:
-                self.__add_guess_to_list(guess)
-                self.__strikes += 1
-                return gameWon
+        return game_won
 
-    def get_game_state(self):
+    def get_game_state(self) -> str:
+        """
+        Get the state of the game.
 
-        # os.system('cls' if os.name == 'nt' else 'clear')
-        state = (
-            f"You have {self.__strikes} strikes.\nWord To Guess: {self.__str__()}\n"
-            + f"Used Letters: {self.__usedLetters}\nUsed Words: {self.__usedWords}"
-        )
-        return state
+        Returns:
+            str: The state of the game.
+        """
 
-    def check_for_winner(self, guess=None, word=None, letterHolder=None):
+        temp = ""
 
-        winner = True
+        for _ , letter in enumerate(self.__letter_holder):
+            temp += letter + " "
 
-        if letterHolder is not None:
+        return self.__strikes, temp
 
-            for letter in letterHolder:
-                if letter == "_":
-                    winner = False
-                    return winner
-            return winner
+    def __check_for_winner(self, guess=None, word=None, letter_holder=None) -> bool:
+        """
+        Checks if a given word matches the word to guess or if the letter_holder
+        variable has no more under scores left.
+
+        Args:
+            guess (str): The guess to check.
+            word (str): The word to check.
+            letter_holder (str): If passed in, checks if there are any underscores left.
+        Returns:
+            bool: True if the word matches or if no underscores are left, False otherwise.
+        """
+
+        if not word:
+            return False
+
+        if not letter_holder:
+            return False
+
+        if not guess:
+
+            winner = True
+
+            if letter_holder is not None:
+
+                for letter in letter_holder:
+                    if letter == "_":
+                        winner = False
+                        return winner
+                return winner
 
         elif guess == "".join(word):
             return winner
@@ -140,33 +197,37 @@ class Hangman:
             winner = False
             return winner
 
-    def play(self):
+    def play(self) -> None:
+        """
+        Sets up the game and begins playing.
+        """
 
-        self.__words, self.__count = self.__get_words_and_wordCount()
+        self.__words, self.__count = self.__get_words_and_word_count()
 
         random.seed()
 
-        self.__wordToGuess = list(self.__words[random.randint(0, self.__count)])
+        self.__word_to_guess = list(self.__words[random.randint(0, self.__count)])
 
-        self.__letterHolder = self.__prepare_game(self.__wordToGuess)
+        self.__letter_holder = self.__prepare_game(self.__word_to_guess)
 
-    def __add_guess_to_list(self, guess):
+    def __add_guess_to_list(self, guess) -> None:
+        """
+        Adds a guessed word or letter to the list of used guesses.
+
+        Args:
+            guess (str): The guessed word or letter.
+        """
 
         if len(guess) == 1:
-            self.__usedLetters.append(guess)
+            self.__used_letters.append(guess)
         else:
-            self.__usedWords.append(guess)
+            self.__used_words.append(guess)
 
     def get_word_to_guess(self):
+        """
+        Returns the word that will be guessed.
 
-        text = ""
-        for letter in self.__wordToGuess:
-            text += letter
-
-        return text
-
-    def get_strikes(self):
-        return self.__strikes
-
-    if __name__ == "__main__":
-        os.system("python main.py")
+        Returns:
+            str: The word that will be guessed.
+        """
+        return self.__word_to_guess
